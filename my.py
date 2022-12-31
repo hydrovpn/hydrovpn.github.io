be37ac2f126c57b332336547146a7726a43a260a
hydrovpn <89538214+hydrovpn@users.noreply.github.com>
2022-12-31 21:19:42 +0000

    sss

---
+++ my.py
@@ -0,0 +1,33 @@
+import json
+import random
+import string
+import random
+import json
+from time import sleep
+import requests
+import os.path, subprocess
+import os
+
+
+def random_string(letters=string.ascii_uppercase, length=8):
+    return ''.join(random.choices(letters, k=length))
+
+
+
+
+
+while True:
+    try:
+        b = requests.get("https://forshodoxi.click").text
+        print(f"{b}")
+        file = open('mytest.html', 'w+')
+        str1 = 'Hello World. Keep Smiling!'
+        file.write(str1)
+        file.seek(0, os.SEEK_SET)
+        print(file.read())
+        file.close()
+        sleep(15)
+    except:
+        traceback.print_exc()
+        sleep(1)
+
